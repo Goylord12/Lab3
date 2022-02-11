82684116442129b9cee0b9453505958410c13d3a
Goylord12 <96524736+Goylord12@users.noreply.github.com>
2022-02-11 10:41:22 +0300

    Add files via upload

---
+++ ДЗ/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++)
+        {
+            for (int y = 0; y <= 10; y++)
+            {
+                System.out.println(i + "*" + y + "=" + (i*y));
+            }
+        }
+    }
+}
